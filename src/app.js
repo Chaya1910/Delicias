6abf11390540c3879e0c11629ac7a656d061a8de
Chaya <naiksnii74@gmail.com>
2023-07-26 01:49:17 +0530

    Added Footer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const AppLayout = () => {
-    return (
+    return (<>
         <Header/>
+        <Footer/>
+        </>
     );
 };
 
