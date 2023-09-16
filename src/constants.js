44519a279418a87712def410385970cf56e79811
Chaya <naiksnii74@gmail.com>
2023-09-17 00:30:58 +0530

    Updated constants

---
+++ src/constants.js
@@ -0,0 +1,390 @@
+export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const SWIGGY_API_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_API =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+
+export const restaurantList = [
+    {
+                "type": "restaurant",
+                "data": {
+                  "type": "F",
+                  "id": "214949",
+                  "name": "The Tiffin Factory - Panaji",
+                  "uuid": "ef44f1cf-1f30-41e6-b6d9-7f592801f0e0",
+                  "city": "63",
+                  "area": "Santa Inez",
+                  "totalRatingsString": "10000+ ratings",
+                  "cloudinaryImageId": "769502dea8c9bffd3256cb99139ed978",
+                  "cuisines": [
+                    "Goan",
+                    "Indian",
+                    "Chinese",
+                    "Thai",
+                    "Continental"
+                  ],
+                  "tags": [
+                    
+                  ],
+                  "costForTwo": 30000,
+                  "costForTwoString": "₹300 FOR TWO",
+                  "deliveryTime": 22,
+                  "minDeliveryTime": 22,
+                  "maxDeliveryTime": 22,
+                  "slaString": "22 MINS",
+                  "lastMileTravel": 2.299999952316284,
+                  "slugs": {
+                    "restaurant": "the-tiffin-factory-panaji-panaji",
+                    "city": "central-goa"
+                  },
+                  "cityState": "63",
+                  "address": "Shop No. G9 and G10, Ground Floor, Noreen Apartment, Opp Gomantak, St. Inez, Panjim Goa, 403001",
+                  "locality": "Panaji",
+                  "parentId": 10246,
+                  "unserviceable": false,
+                  "veg": false,
+                  "select": false,
+                  "favorite": false,
+                  "tradeCampaignHeaders": [
+                    
+                  ],
+                  "chain": [
+                    
+                  ],
+                  "feeDetails": {
+                    "fees": [
+                      {
+                        "name": "distance",
+                        "fee": 2400,
+                        "message": ""
+                      },
+                      {
+                        "name": "time",
+                        "fee": 0,
+                        "message": ""
+                      },
+                      {
+                        "name": "special",
+                        "fee": 0,
+                        "message": ""
+                      }
+                    ],
+                    "totalFees": 2400,
+                    "message": "",
+                    "title": "Delivery Charge",
+                    "amount": "2400",
+                    "icon": ""
+                  },
+                  "availability": {
+                    "opened": true,
+                    "nextOpenMessage": "",
+                    "nextCloseMessage": ""
+                  },
+                  "longDistanceEnabled": 0,
+                  "rainMode": "NONE",
+                  "thirdPartyAddress": false,
+                  "thirdPartyVendor": "",
+                  "adTrackingID": "",
+                  "badges": {
+                    "imageBased": [
+                      
+                    ],
+                    "textBased": [
+                      
+                    ],
+                    "textExtendedBadges": [
+                      
+                    ]
+                  },
+                  "lastMileTravelString": "2.2 kms",
+                  "hasSurge": false,
+                  "aggregatedDiscountInfoV3": {
+                    "header": "50% OFF",
+                    "subHeader": "UPTO ₹100",
+                    "discountTag": "",
+                    "headerTypeV2": 0
+                  },
+                  "loyaltyDiscoverPresentationInfo": {
+                    "logoCtx": {
+                      "text": "BENEFITS",
+                      "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+                    },
+                    "freedelMessage": "FREE DELIVERY",
+                    "exclusiveOfferMessage": ""
+                  },
+                  "sla": {
+                    "restaurantId": "214949",
+                    "deliveryTime": 22,
+                    "minDeliveryTime": 22,
+                    "maxDeliveryTime": 22,
+                    "lastMileTravel": 2.299999952316284,
+                    "lastMileDistance": 0,
+                    "serviceability": "SERVICEABLE",
+                    "rainMode": "NONE",
+                    "longDistance": "NOT_LONG_DISTANCE",
+                    "preferentialService": false,
+                    "iconType": "EMPTY"
+                  },
+                  "promoted": false,
+                  "avgRating": "4.0",
+                  "totalRatings": 10000,
+                  "new": false
+                },
+                "subtype": "basic"
+              },
+              {
+                "type": "restaurant",
+                "data": {
+                  "type": "F",
+                  "id": "483519",
+                  "name": "Cafe Tato - Patto",
+                  "uuid": "0f5c2fbd-cee6-4a77-a3f4-bd7be1dea83f",
+                  "city": "63",
+                  "area": "Panaji",
+                  "totalRatingsString": "1000+ ratings",
+                  "cloudinaryImageId": "s9zor4hjliueeofgunmv",
+                  "cuisines": [
+                    "Goan",
+                    "Indian",
+                    "South Indian",
+                    "Snacks"
+                  ],
+                  "tags": [
+                    
+                  ],
+                  "costForTwo": 50000,
+                  "costForTwoString": "₹500 FOR TWO",
+                  "deliveryTime": 13,
+                  "minDeliveryTime": 13,
+                  "maxDeliveryTime": 13,
+                  "slaString": "13 MINS",
+                  "lastMileTravel": 0.6000000238418579,
+                  "slugs": {
+                    "restaurant": "cafe-tato-panaji-panaji",
+                    "city": "central-goa"
+                  },
+                  "cityState": "63",
+                  "address": "1/280/1 shop no g9 gera imperim plot no 8 complex panaji tiswadi north go 403001",
+                  "locality": "Gera Imperim",
+                  "parentId": 290430,
+                  "unserviceable": false,
+                  "veg": false,
+                  "select": false,
+                  "favorite": false,
+                  "tradeCampaignHeaders": [
+                    
+                  ],
+                  "chain": [
+                    
+                  ],
+                  "feeDetails": {
+                    "fees": [
+                      {
+                        "name": "distance",
+                        "fee": 2400,
+                        "message": ""
+                      },
+                      {
+                        "name": "time",
+                        "fee": 0,
+                        "message": ""
+                      },
+                      {
+                        "name": "special",
+                        "fee": 0,
+                        "message": ""
+                      }
+                    ],
+                    "totalFees": 2400,
+                    "message": "",
+                    "title": "Delivery Charge",
+                    "amount": "2400",
+                    "icon": ""
+                  },
+                  "availability": {
+                    "opened": true,
+                    "nextOpenMessage": "",
+                    "nextCloseMessage": ""
+                  },
+                  "longDistanceEnabled": 0,
+                  "rainMode": "NONE",
+                  "thirdPartyAddress": false,
+                  "thirdPartyVendor": "",
+                  "adTrackingID": "",
+                  "badges": {
+                    "imageBased": [
+                      
+                    ],
+                    "textBased": [
+                      
+                    ],
+                    "textExtendedBadges": [
+                      
+                    ]
+                  },
+                  "lastMileTravelString": "0.6 kms",
+                  "hasSurge": false,
+                  "aggregatedDiscountInfoV3": {
+                    "header": "60% OFF",
+                    "subHeader": "UPTO ₹120",
+                    "discountTag": "FOR YOU",
+                    "headerTypeV2": 1
+                  },
+                  "loyaltyDiscoverPresentationInfo": {
+                    "logoCtx": {
+                      "text": "BENEFITS",
+                      "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+                    },
+                    "freedelMessage": "FREE DELIVERY",
+                    "exclusiveOfferMessage": ""
+                  },
+                  "sla": {
+                    "restaurantId": "483519",
+                    "deliveryTime": 13,
+                    "minDeliveryTime": 13,
+                    "maxDeliveryTime": 13,
+                    "lastMileTravel": 0.6000000238418579,
+                    "lastMileDistance": 0,
+                    "serviceability": "SERVICEABLE",
+                    "rainMode": "NONE",
+                    "longDistance": "NOT_LONG_DISTANCE",
+                    "preferentialService": false,
+                    "iconType": "EMPTY"
+                  },
+                  "promoted": false,
+                  "avgRating": "4.3",
+                  "totalRatings": 1000,
+                  "new": false
+                },
+                "subtype": "basic"
+              },
+              {
+                "type": "restaurant",
+                "data": {
+                  "type": "F",
+                  "id": "644785",
+                  "name": "Taco Bell",
+                  "uuid": "ab6a92cb-15de-415c-b2a0-0e81fbd936a6",
+                  "city": "63",
+                  "area": "Miramar",
+                  "totalRatingsString": "500+ ratings",
+                  "cloudinaryImageId": "e7f40335a66b230f5eda766022dfecbd",
+                  "cuisines": [
+                    "Mexican"
+                  ],
+                  "tags": [
+                    
+                  ],
+                  "costForTwo": 30000,
+                  "costForTwoString": "₹300 FOR TWO",
+                  "deliveryTime": 25,
+                  "minDeliveryTime": 25,
+                  "maxDeliveryTime": 25,
+                  "slaString": "25 MINS",
+                  "lastMileTravel": 3,
+                  "slugs": {
+                    "restaurant": "taco-bell-panjim-panaji",
+                    "city": "central-goa"
+                  },
+                  "cityState": "63",
+                  "address": "Taco Bell, Burman Hospitality Pvt Ltd, Ground Floor, Kamala House, Dayanand Bandodkar Marg, Miramar, Panjim, Goa-403001",
+                  "locality": "Panjim",
+                  "parentId": 1557,
+                  "unserviceable": false,
+                  "veg": false,
+                  "select": false,
+                  "favorite": false,
+                  "tradeCampaignHeaders": [
+                    
+                  ],
+                  "ribbon": [
+                    {
+                      "type": "PROMOTED"
+                    }
+                  ],
+                  "chain": [
+                    
+                  ],
+                  "feeDetails": {
+                    "fees": [
+                      {
+                        "name": "distance",
+                        "fee": 3100,
+                        "message": ""
+                      },
+                      {
+                        "name": "time",
+                        "fee": 0,
+                        "message": ""
+                      },
+                      {
+                        "name": "special",
+                        "fee": 0,
+                        "message": ""
+                      }
+                    ],
+                    "totalFees": 3100,
+                    "message": "",
+                    "title": "Delivery Charge",
+                    "amount": "3100",
+                    "icon": ""
+                  },
+                  "availability": {
+                    "opened": true,
+                    "nextOpenMessage": "",
+                    "nextCloseMessage": ""
+                  },
+                  "longDistanceEnabled": 0,
+                  "rainMode": "NONE",
+                  "thirdPartyAddress": false,
+                  "thirdPartyVendor": "",
+                  "adTrackingID": "cid=6999855~p=4~eid=00000188-d775-e1a0-01e1-8e95002b0458~srvts=1687242006944",
+                  "badges": {
+                    "imageBased": [
+                      
+                    ],
+                    "textBased": [
+                      
+                    ],
+                    "textExtendedBadges": [
+                      
+                    ]
+                  },
+                  "lastMileTravelString": "3 kms",
+                  "hasSurge": false,
+                  "aggregatedDiscountInfoV3": {
+                    "header": "40% OFF",
+                    "subHeader": "UPTO ₹80",
+                    "discountTag": "",
+                    "headerTypeV2": 0
+                  },
+                  "loyaltyDiscoverPresentationInfo": {
+                    "logoCtx": {
+                      "text": "BENEFITS",
+                      "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+                    },
+                    "freedelMessage": "FREE DELIVERY",
+                    "exclusiveOfferMessage": ""
+                  },
+                  "sla": {
+                    "restaurantId": "644785",
+                    "deliveryTime": 25,
+                    "minDeliveryTime": 25,
+                    "maxDeliveryTime": 25,
+                    "lastMileTravel": 3,
+                    "lastMileDistance": 0,
+                    "serviceability": "SERVICEABLE",
+                    "rainMode": "NONE",
+                    "longDistance": "NOT_LONG_DISTANCE",
+                    "preferentialService": false,
+                    "iconType": "EMPTY"
+                  },
+                  "promoted": true,
+                  "avgRating": "3.8",
+                  "totalRatings": 500,
+                  "new": false
+                },
+                "subtype": "basic"
+              },
+];
