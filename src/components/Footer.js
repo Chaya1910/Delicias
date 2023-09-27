6729a1a0acead2ac866af1275bd828b8b6154fef
Chaya <naiksnii74@gmail.com>
2023-09-27 13:21:14 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,13 @@
 const Footer = () => {
     return (
-        <h4>Footer</h4>        
+        <>
+        <div className="w-full h-18 p-4 bg-black flex justify-center items-center fixed bottom-0">
+            <h1 className="text-white font-semibold">
+                Made with ❤ by Chaya
+            </h1>
+
+        </div>
+        </>
     );
 };
 
