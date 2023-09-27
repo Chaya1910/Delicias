cbbfec74f9430e3e619f9038bb1b6f31209e5408
Chaya <naiksnii74@gmail.com>
2023-09-27 13:20:53 +0530

    Updated About Us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
-import User from "./User";
-import UserClass from "./UserClass";
 import { Component } from "react";
+import Bg1 from '../assets/img/Bg1.jpg';
 
 class About extends Component {
   constructor(props) {
@@ -9,27 +8,25 @@ class About extends Component {
 }
 
 componentDidMount() {
-    //console.log("Parent Component Did Mount");
 }
 
 render() {
-    //console.log("Parent Render");
     return (
-      <div>
-        <h1>About Class Component</h1>
-        <h2>This is Namaste React Web Series</h2>
-        <UserClass name={"First"} location={"Dehradun Class"} />
+      <div className="flex flex-col" >
+        <img className="h-screen w-full" src={Bg1}>
+
+        </img>
+        <div className="text-white absolute top-56 w-[500px] ml-28">
+        <h2 className="font-bold text-xl mb-2">What’s In Store For The Future?</h2>
+        <p className="font-semibold text-lg">Swiggy has grand plans to be India’s most loved hyperlocal player.
+        It aims to be the most accessible platform on the network - reimagining
+         the meaning of convenience in the country through a variety of service offerings.
+         </p>
+        </div>
       </div>
     );
   }
 }
 
 
-// const About = () => {
-//     return (<div>
-//         <h1>About us</h1>
-//         <p>This is about us page</p>
-//     </div>)
-// }
-
-// export default About;
+export default About;
