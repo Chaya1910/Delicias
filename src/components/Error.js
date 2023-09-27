f36956d534fa799406e4005e3b4eb20a84bdae98
Chaya <naiksnii74@gmail.com>
2023-09-27 13:18:50 +0530

    Updated Error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,16 +1,17 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
+import ErrorIcon from '../assets/img/ErrorIcon.png';
 
 const Error = () => {
+  const err = useRouteError();
 
-    const err = useRouteError();
-    console.log(err);
-
-    return (
-        <>
-        <h1>Oops!!</h1>
-        <p>Something went wrong</p>
-        <h2>{err.status + " " + err.statusText}</h2></>
-    )
-}
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+    <img className="h-48 w-48 inline-block" src={ErrorIcon} alt="logo"></img>
+      <h1 className="font-bold text-md">Oops, Something went wrong!!</h1>
+      <p className="font-light text-sm">Please try to refresh the page</p>
+      <h2 className="font-light text-sm">{err.status + " " + err.statusText}</h2>
+    </div>
+  );
+};
 
 export default Error;
